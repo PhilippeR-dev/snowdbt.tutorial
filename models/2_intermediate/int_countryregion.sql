a348c6807a411b77af23f83d543b41d03e22af73
Philippe REY <prey@synergy.fr>
2024-02-13 13:50:08 +0100

    suppr

--- models/2_intermediate/int_countryregion.sql
+++
@@ -1,8 +0,0 @@
-WITH countryregion AS (
-  SELECT * FROM {{ ref('stg_raw_data__countryregion') }}
-)
-SELECT
-  countryregioncode,
-  CAST(name AS VARCHAR(50)) AS countryregionname,
-  modifieddate
-FROM countryregion
